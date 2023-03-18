41fcf301ff25a5e6328f66af923ed46d889ec8c0
wybran <krystian.wybranowski@gmail.com>
2023-03-18 20:53:46 +0100

    fe: basic form with react-hook-form

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,30 +1,79 @@
+"use client";
+import { useForm, SubmitHandler } from "react-hook-form";
+
+type Inputs = {
+    IP: string;
+    username: string;
+    password: string;
+    port: number;
+    command: string;
+};
 
 export default function Home() {
+    const {
+        register,
+        handleSubmit
+    } = useForm<Inputs>();
+    const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
+
     return (
         <main>
             <h1>Connect to SSH server</h1>
-            <form>
+            <form onSubmit={handleSubmit(onSubmit)}>
                 <div className="form-group">
                     <label htmlFor="IP">IP</label>
-                    <input type="text" className="form-control" id="IP" placeholder="Enter IP address" />
-                </div> 
+                    <input
+                        {...register("IP", { required: true })}
+                        type="text"
+                        className="form-control"
+                        id="IP"
+                        placeholder="Enter IP address"
+                    />
+                </div>
                 <div className="form-group">
                     <label htmlFor="username">Username</label>
-                    <input type="text" className="form-control" id="username" placeholder="Enter username" />
+                    <input
+                        {...register("username", { required: true })}
+                        type="text"
+                        className="form-control"
+                        id="username"
+                        placeholder="Enter username"
+                    />
                 </div>
                 <div className="form-group">
                     <label htmlFor="password">Password</label>
-                    <input type="password" className="form-control" id="password" placeholder="Enter password" />
+                    <input
+                        {...register("password", { required: true })}
+                        type="password"
+                        className="form-control"
+                        id="password"
+                        placeholder="Enter password"
+                    />
                 </div>
                 <div className="form-group">
                     <label htmlFor="port">Port</label>
-                    <input type="number" className="form-control" id="port" placeholder="Enter port" defaultValue={22}/>
+                    <input
+                        {...register("port", { required: true })}
+                        type="number"
+                        className="form-control"
+                        id="port"
+                        placeholder="Enter port"
+                        defaultValue={22}
+                    />
                 </div>
                 <div className="form-group">
                     <label htmlFor="command">Command</label>
-                    <input type="text" className="form-control" id="command" placeholder="Enter command" />
+                    <input
+                        {...register("command", { required: true })}
+                        type="text"
+                        className="form-control"
+                        id="command"
+                        placeholder="Enter command"
+                    />
                 </div>
-                <button type="submit" className="btn btn-primary">Submit</button>
+                <button type="submit" className="btn btn-primary">
+                    Submit
+                </button>
             </form>
         </main>
     );
